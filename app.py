43805df3902bc85af9d59f227d95e65751220648
gopher-blueberry <diego.martinezlandry@gmail.com>
2025-06-06 20:17:10 +0000

    ADDED JINGA

--- app.py
+++ app.py
@@ -2,41 +2,23 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-  {
+USERS = [{
     'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$150,000'
-  }
-]
+    'username': '______',
+    'email': '______',
+    'password': '_____'
+}]
+
 
 @app.route("/")
 def hello_jovian():
-    return render_template('home.html', 
-                           jobs=JOBS, 
-                           company_name='Jovian')
+  return render_template('home.html', username=USERS, company_name='AIO')
 
-@app.route("/api/jobs")
-def list_jobs():
-  return jsonify(JOBS)
+
+@app.route("/api/usernames")
+def list_users():
+  return jsonify(USERS)
+#Para datos en formato JSON
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
